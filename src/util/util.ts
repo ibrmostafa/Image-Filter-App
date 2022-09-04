ceb07e069d18ba663a365777efe231aa78572a83
Mostafa <mostafa-ibrahim@hotmail.com>
2022-09-04 21:20:30 +0200

    Update util.ts

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,7 @@
 import fs from "fs";
 import Jimp = require("jimp");
+import axios from "axios";
+import { AxiosResponse } from "axios";
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -11,6 +13,9 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
+      const { data } = await axios.get<any, AxiosResponse<ArrayBuffer>>(inputURL, {    responseType: "arraybuffer",      });
+      const imageBuffer = Buffer.from(data);
+      const photo = await Jimp.read(imageBuffer);
       const photo = await Jimp.read(inputURL);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
